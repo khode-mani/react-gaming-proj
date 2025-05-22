dea9c6d5a9c365d943602588b72e396fdf254e7d
mani <mani.norouzi.130084@gmail.com>
2025-05-22 09:12:04 +0330

    another base

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/react-gaming-proj/", // مسیر را مطابق نام مخزن تنظیم کن
+  base: "https://github.com/khode-mani/react-gaming-proj", 
   plugins: [react()],
 })
