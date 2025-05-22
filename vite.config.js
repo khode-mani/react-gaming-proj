52368f5f5a78587ac08c53f1c147cbb815d34f6d
mani <mani.norouzi.130084@gmail.com>
2025-05-22 09:17:00 +0330

    restore

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "https://github.com/khode-mani/react-gaming-proj", 
+  // base: "https://github.com/khode-mani/react-gaming-proj", 
   plugins: [react()],
 })
